5b882a45ea6859c5fb1f426045d59b44c9783126
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-12-05 21:23:08 +0100

    Replaced qa_process by a very rudimentary implementation (simply remove stop words)

--- qa/src/components/question_processing/main.py
+++ qa/src/components/question_processing/main.py
@@ -1,11 +1,13 @@
 from models.answer_type import AnswerType
 from models.qp_result import QPResult
 from models.question_model import QuestionModel
+from utils.nlptoolkit import NLPToolkit
 from .fit_predict import get_clf_from_disk, get_predicted_label, get_clf_name
 
 
-def process_question(question: str) -> QPResult:
-    return QPResult(QuestionModel(question.split(" ")), AnswerType.ENTY)
+def process_question(question: str, nlp_toolkit: NLPToolkit) -> QPResult:
+    question_keywords = nlp_toolkit.extract_stop_words(question)[1:]
+    return QPResult(QuestionModel(question_keywords), AnswerType.ENTY)
     # clf_name = get_clf_name(question)
     # clf = get_clf_from_disk(clf_name)
     # label = get_predicted_label(question, clf)
