479b9e8062d638904d97e02f1432328197591cbd
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-12-05 20:58:12 +0100

    Disabled question_analysis since it doesn't work at my machine :-(
    Maybe the bug sits in front of the pc

--- qa/src/components/question_processing/main.py
+++ qa/src/components/question_processing/main.py
@@ -3,8 +3,10 @@ from models.qp_result import QPResult
 from models.question_model import QuestionModel
 from .fit_predict import get_clf_from_disk, get_predicted_label, get_clf_name
 
+
 def process_question(question: str) -> QPResult:
-    clf_name = get_clf_name(question)
-    clf = get_clf_from_disk(clf_name)
-    label = get_predicted_label(question, clf)
-    return QPResult(QuestionModel(question.split()), AnswerType[label])
+    return QPResult(QuestionModel(question.split(" ")), AnswerType.ENTY)
+    # clf_name = get_clf_name(question)
+    # clf = get_clf_from_disk(clf_name)
+    # label = get_predicted_label(question, clf)
+    # return QPResult(QuestionModel(question.split()), AnswerType[label])
