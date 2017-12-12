a3ff507de7e6ffca42dc9d9bce6e759f6606896b
mreimsbach <mreimsbach@avarteq.de>
2017-12-12 21:03:02 +0100

    add keywords and named_entities

--- qa/src/components/passage_retrieval/passage_classifier.py
+++ qa/src/components/passage_retrieval/passage_classifier.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 from sklearn.svm import SVC
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
@@ -8,6 +9,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
+sys.path.insert(0,'~/entwicklung/twitter-bot')
+from components.question_processing import process_question
+from utils.nlptoolkit import NLPToolkit
 
 import spacy
 
@@ -41,21 +45,40 @@ def split_trainingsdata_into_sentences():
         sentences = entry['text'].split('.')
         for qas in entry['qas']:
             question = qas['question']
-            relevant_sentences = []
-            not_relevant_sentences = []
+            qp_result = process_question(question, NLPToolkit())
+            #relevant_sentences = []
+            #not_relevant_sentences = []
             for sentence in sentences:
                 relevant = False
                 for answer in qas['answers']:
                     if answer in sentence:
                         relevant = True
-                f.write(question + " " + sentence + " ::: " + str(relevant) + '\n')
+                    keywords = get_number_of_keywords(sentence, qp_result.question_model.get_keywords())
+                    named_entities = get_number_of_named_entities(sentence, qp_result.answer_type)
+                    print("Keywords: " + str(keywords))
+                    print("NE: " + str(named_entities))
+                    f.write(question + " " + sentence + " ::: " + str(relevant) + '\n')
          
             #trainings_data_sentence.append({'question': question, 'answers': qas, 'relevant': relevant_sentences, 'not_relevant': not_relevant_sentences})
     #with open('trainings_data_sentence.json', 'w') as outfile:
      #   json.dump({'data': trainings_data_sentence}, outfile)   
                  
-#split_trainingsdata_into_sentences()
+def get_number_of_keywords(passage, keywords):
+    count = 0
+    passage_words = passage.spllit()
+    for keyword in keywords:
+        count = count + passage_words.count(keyword)
+    return count 
 
+def get_number_of_named_entities(passage, entity):
+    count = 0
+    doc = nlp(passage)
+    for ent in doc.ents:
+        if ent.label_ == entity:
+            count = count + 1
+    return count
+        
+        
 def get_most_similar(passages, question):
     nlp = spacy.load('en_core_web_lg')
 
@@ -87,21 +110,22 @@ def get_passage(passages, question):
             print()
     
 def main():
-    sentence_train = pd.read_csv('sentence_train', header=None, encoding='utf-8', sep=':::', engine='python')
-    x = sentence_train.iloc[:,0].values
-    nb = Pipeline([('vect', CountVectorizer()), ('clf', MultinomialNB(alpha=1.0))])
-    y = sentence_train.iloc[:, 1].values
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    nb.fit(X_train, y_train)
-    y_predicted = nb.predict(X_test)
+    split_trainingsdata_into_sentences()
+    # sentence_train = pd.read_csv('sentence_train', header=None, encoding='utf-8', sep=':::', engine='python')
+    # x = sentence_train.iloc[:,0].values
+    # nb = Pipeline([('vect', CountVectorizer()), ('clf', MultinomialNB(alpha=1.0))])
+    # y = sentence_train.iloc[:, 1].values
+    # X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+    # nb.fit(X_train, y_train)
+    # y_predicted = nb.predict(X_test)
 
-    joblib.dump(nb, os.path.join(DIR, "nb" + '.pkl'), compress=9)
-    ## Gütemaße ausgeben
-    print("Korrektklassifizierungsrate:\n", accuracy_score(y_true=y_test, y_pred=y_predicted))
-    print("Präzision (mikro):\n", precision_score(y_true=y_test, y_pred=y_predicted, average='micro'))
-    print("Ausbeute (mikro):\n", recall_score(y_true=y_test, y_pred=y_predicted, average='micro'))
-    #print("F1 (mikro):\n", f1_score(y_true=y_test, y_pred=y_predicted, average='micrgio'))
-    print("Kofusionsmatrix:\n", confusion_matrix(y_true=y_test, y_pred=y_predicted))
+    # joblib.dump(nb, os.path.join(DIR, "nb" + '.pkl'), compress=9)
+    # ## Gütemaße ausgeben
+    # print("Korrektklassifizierungsrate:\n", accuracy_score(y_true=y_test, y_pred=y_predicted))
+    # print("Präzision (mikro):\n", precision_score(y_true=y_test, y_pred=y_predicted, average='micro'))
+    # print("Ausbeute (mikro):\n", recall_score(y_true=y_test, y_pred=y_predicted, average='micro'))
+    # #print("F1 (mikro):\n", f1_score(y_true=y_test, y_pred=y_predicted, average='micrgio'))
+    # print("Kofusionsmatrix:\n", confusion_matrix(y_true=y_test, y_pred=y_predicted))
 
 if __name__ == "__main__":
     main()
