ce1e49ba42543eb35a737c7162b650cc5b4bf477
mreimsbach <mreimsbach@avarteq.de>
2017-12-16 16:57:57 +0100

    add keywords feature and similarity feature

--- qa/src/components/passage_retrieval/passage_classifier.py
+++ qa/src/components/passage_retrieval/passage_classifier.py
@@ -1,6 +1,11 @@
 import os
 import json
 import sys
+import sys
+import os
+DIR = os.path.dirname(__file__)
+
+
 from sklearn.svm import SVC
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
@@ -9,13 +14,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
-sys.path.insert(0,'~/entwicklung/twitter-bot')
 from components.question_processing import process_question
 from utils.nlptoolkit import NLPToolkit
+from models.answer_type import AnswerType
 
 import spacy
 
-DIR = os.path.dirname(__file__)
+#DIR = os.path.dirname(__file__)
+nlp = spacy.load('en')
 
 def prepare_trainingsdata():
     data = json.load(open('train-v1.1.json'))
@@ -48,24 +54,31 @@ def split_trainingsdata_into_sentences():
             qp_result = process_question(question, NLPToolkit())
             #relevant_sentences = []
             #not_relevant_sentences = []
+            keywords = qp_result.question_model.get_keywords()
+            answer_type = qp_result.answer_type
+            #print(answer_type)
             for sentence in sentences:
                 relevant = False
                 for answer in qas['answers']:
                     if answer in sentence:
                         relevant = True
-                    keywords = get_number_of_keywords(sentence, qp_result.question_model.get_keywords())
-                    named_entities = get_number_of_named_entities(sentence, qp_result.answer_type)
-                    print("Keywords: " + str(keywords))
-                    print("NE: " + str(named_entities))
-                    f.write(question + " " + sentence + " ::: " + str(relevant) + '\n')
+                    count_keywords = get_number_of_keywords(sentence, keywords)
+                    similarity = get_similiarity(question, answer)
+                    #count_named_entities = get_number_of_named_entities(sentence, answer_type)
+                    #print("Keywords: " + str(keywords))
+                    #print("NE: " + str(named_entities))
+                    f.write(question + " " + sentence + " " + str(count_keywords) + " " + str(similarity) +  " ::: " + str(relevant) + '\n')
          
             #trainings_data_sentence.append({'question': question, 'answers': qas, 'relevant': relevant_sentences, 'not_relevant': not_relevant_sentences})
     #with open('trainings_data_sentence.json', 'w') as outfile:
      #   json.dump({'data': trainings_data_sentence}, outfile)   
-                 
+def get_similiarity(question, answer):
+    q_doc = nlp(question)
+    q_ans = nlp(answer)
+    return q_doc.similarity(q_ans)          
 def get_number_of_keywords(passage, keywords):
     count = 0
-    passage_words = passage.spllit()
+    passage_words = passage.split()
     for keyword in keywords:
         count = count + passage_words.count(keyword)
     return count 
@@ -110,22 +123,23 @@ def get_passage(passages, question):
             print()
     
 def main():
-    split_trainingsdata_into_sentences()
-    # sentence_train = pd.read_csv('sentence_train', header=None, encoding='utf-8', sep=':::', engine='python')
-    # x = sentence_train.iloc[:,0].values
-    # nb = Pipeline([('vect', CountVectorizer()), ('clf', MultinomialNB(alpha=1.0))])
-    # y = sentence_train.iloc[:, 1].values
-    # X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    # nb.fit(X_train, y_train)
-    # y_predicted = nb.predict(X_test)
+    #split_trainingsdata_into_sentences()
+    sentence_train = pd.read_csv('sentence_train', header=None, encoding='utf-8', sep=':::', engine='python')
+    x = sentence_train.iloc[:,0].values
+    nb = Pipeline([('vect', CountVectorizer()), ('clf', MultinomialNB(alpha=1.0))])
+    y = sentence_train.iloc[:, 1].values
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+    nb.fit(X_train, y_train)
+    y_predicted = nb.predict(X_test)
 
-    # joblib.dump(nb, os.path.join(DIR, "nb" + '.pkl'), compress=9)
-    # ## Gütemaße ausgeben
-    # print("Korrektklassifizierungsrate:\n", accuracy_score(y_true=y_test, y_pred=y_predicted))
-    # print("Präzision (mikro):\n", precision_score(y_true=y_test, y_pred=y_predicted, average='micro'))
-    # print("Ausbeute (mikro):\n", recall_score(y_true=y_test, y_pred=y_predicted, average='micro'))
-    # #print("F1 (mikro):\n", f1_score(y_true=y_test, y_pred=y_predicted, average='micrgio'))
-    # print("Kofusionsmatrix:\n", confusion_matrix(y_true=y_test, y_pred=y_predicted))
+    joblib.dump(nb, os.path.join(DIR, "nb" + '.pkl'), compress=9)
+    ## Gütemaße ausgeben
+    print("Korrektklassifizierungsrate:\n", accuracy_score(y_true=y_test, y_pred=y_predicted))
+    print("Präzision (mikro):\n", precision_score(y_true=y_test, y_pred=y_predicted, average='micro'))
+    print("Ausbeute (mikro):\n", recall_score(y_true=y_test, y_pred=y_predicted, average='micro'))
+    print("F1 (mikro):\n", f1_score(y_true=y_test, y_pred=y_predicted, average='micro'))
+    print("Kofusionsmatrix:\n", confusion_matrix(y_true=y_test, y_pred=y_predicted))
+    print("Konfusionsmatrix: \n", pd.crosstab(y_test, y_predicted))
 
 if __name__ == "__main__":
     main()
