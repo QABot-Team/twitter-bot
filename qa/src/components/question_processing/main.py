c8b1c10fd47c0334fc1321f8e0d07f4dbc6f9b2a
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-01-08 20:14:03 +0100

    process_question: remove unused parameter

--- qa/src/components/question_processing/main.py
+++ qa/src/components/question_processing/main.py
@@ -1,12 +1,11 @@
 from models.answer_type import AnswerType
 from models.qp_result import QPResult
 from models.question_model import QuestionModel
-from utils.nlptoolkit import NLPToolkit
 from utils.logger import Logger
 from .fit_predict import get_clf_from_disk, get_predicted_label, get_clf_name, get_key_words
 
 
-def process_question(question: str, nlpToolik: NLPToolkit) -> QPResult:
+def process_question(question: str) -> QPResult:
     Logger.info('started')
     clf_name = get_clf_name(question)
     clf = get_clf_from_disk(clf_name)
