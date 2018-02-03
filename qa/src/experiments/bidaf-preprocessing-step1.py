14b00cc0f5b4686157d45260ff5adb54675307cb
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-02-03 13:07:01 +0100

    Do post-processing manually
    The bidaf forward prediction returns the best_span of the passage. This will be converted into the answer as a string.

--- qa/src/experiments/bidaf-preprocessing-step1.py
+++ qa/src/experiments/bidaf-preprocessing-step1.py
@@ -30,21 +30,21 @@ def tokenize(string):
     doc = nlp(string)
     tokens = []
     for token in doc:
-        tokens.append(str(token))
+        tokens.append(token)
     return tokens
 
 
 def tokens_to_tensor(tokens):
     indizes = []
     for token in tokens:
-        token_index = vocabulary.get_token_index(token.lower())
+        token_index = vocabulary.get_token_index(token.text.lower())
         indizes.append(token_index)
     return Variable(torch.LongTensor([indizes]))
 
 
 def token_to_charnum_list(token, length):
     chars = [259]
-    for c in token:
+    for c in token.text:
         chars.append(ord(c) + 1)
     chars.append(260)
     chars += [0] * (length - len(chars))
@@ -59,6 +59,22 @@ def tokens_to_char_tensor(tokens):
     return Variable(torch.LongTensor([list_of_vectors]))
 
 
+def get_token_offsets(tokens):
+    offsets = []
+    for token in tokens:
+        start = token.idx
+        offset = (start, start + len(token.text))
+        offsets.append(offset)
+    return offsets
+
+
+def best_span_to_answer(best_spans, offsets):
+    predicted_span = tuple(best_spans[0].data.cpu().numpy())
+    start_offset = offsets[predicted_span[0]][0]
+    end_offset = offsets[predicted_span[1]][1]
+    return passage_str[start_offset:end_offset]
+
+
 passage_str = "A reusable launch system (RLS, or reusable launch vehicle, RLV) is a launch system which is capable " \
           "of launching a payload into space more than once. This contrasts with expendable launch systems, where " \
           "each launch vehicle is launched once and then discarded. No completely reusable orbital launch system has " \
@@ -68,23 +84,22 @@ passage_str = "A reusable launch system (RLS, or reusable launch vehicle, RLV) i
           "months of refitting work for each launch. The external tank was discarded after each flight."
 question_str = "How many partially reusable launch systems were developed?"
 
-p_str_tokens = tokenize(passage_str)
-q_str_tokens = tokenize(question_str)
+p_tokens = tokenize(passage_str)
+q_tokens = tokenize(question_str)
 
-
-p_tokens = tokens_to_tensor(p_str_tokens)
-q_tokens = tokens_to_tensor(q_str_tokens)
 
 passage = {
-    'tokens': p_tokens,
-    'token_characters': tokens_to_char_tensor(p_str_tokens)
+    'tokens': tokens_to_tensor(p_tokens),
+    'token_characters': tokens_to_char_tensor(p_tokens)
 }
 
 question = {
-    'tokens': q_tokens,
-    'token_characters': tokens_to_char_tensor(q_str_tokens)
+    'tokens': tokens_to_tensor(q_tokens),
+    'token_characters': tokens_to_char_tensor(q_tokens)
 }
 
 res = archive.model.forward(question, passage)
 
-print('fertig')
+answer = best_span_to_answer(res['best_span'], get_token_offsets(p_tokens))
+
+print(answer)
