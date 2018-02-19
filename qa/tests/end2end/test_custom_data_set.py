0f4e998ee5cb4bacdbb26c9ba14741aaf4ceda48
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-02-19 17:04:36 +0100

    Improved test for custom dataset

--- qa/tests/end2end/test_custom_data_set.py
+++ qa/tests/end2end/test_custom_data_set.py
@@ -9,15 +9,7 @@ from qa_process_impl import process_answer_question
 
 from utils.logger import Logger
 
-from components.document_retrieval.main import receive_docs
 
-from utils.nlptoolkit import NLPToolkit
-from models.question_model import QuestionModel
-from components.question_processing.main import process_question
-from components.passage_retrieval.main import receive_passages
-from components.answer_processing.main import process_answer
-
-#data = json.load(open('dev-v1.1.json'))
 data = json.load(open('custom_data_set.json'))
 
 
@@ -39,12 +31,7 @@ Logger.config('error')
 
 Logger.info("Start document_retrieval analysis")
 
-nlpToolkit = NLPToolkit()
 question_counter = 0
-correct_article_counter = 0
-correct_firstArticle_counter = 0
-correct_secondArticle_counter = 0
-correct_thirdArticle_counter = 0
 correct_answers_counter = 0
 
 for dataset in data['data']:
@@ -53,7 +40,6 @@ for dataset in data['data']:
 
     for paragraph in dataset['paragraphs'][:5]:
         for question_answer_set in paragraph['qas']:
-            question_counter += 1
             question = question_answer_set['question']
             correct_answers = question_answer_set['answers']
 
@@ -66,9 +52,11 @@ for dataset in data['data']:
             if text_contains_any_answer(answer, correct_answers):
                 correct_answers_counter += 1
 
+            question_counter += 1
+
             Logger.error('')
             Logger.error('')
-            Logger.error('Right Answers: ' + str(correct_answers_counter) + " / " + str(correct_article_counter))
+            Logger.error('Right Answers: ' + str(correct_answers_counter) + " / " + str(question_counter))
             Logger.error('')
-            Logger.error('##############################################################################################')
+            Logger.error('############################################################################################')
             Logger.error('')
